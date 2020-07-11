a9323cfa16e3b1afb1cca7a66b7694880a7504c1
Bsacco9814 <Brandon.Sacco13@gmail.com>
2020-07-11 12:04:47 -0500

    trying to get something to display

---
+++ src/components/Weatherforcast/Weatherforcast.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Weatherforcast.css"
+
+
+export default function Weatherforcast() {
+    return (
+              <div className="weatherforcast">
+                <main>
+                  <div className="search-box">
+                    <input type="text" className="search-bar" placeholder="Search..." />
+                  </div>
+                </main>
+              </div>
+            );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,44 +1,51 @@
-import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom'
-import './App.css';
-import NavBar from '../../components/NavBar/NavBar';
-import LoginPage from '../LoginPage/LoginPage';
-import SignupPage from '../SignupPage/SignupPage';
-import userService from '../../services/userService';
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "../../components/NavBar/NavBar";
+import LoginPage from "../LoginPage/LoginPage";
+import SignupPage from "../SignupPage/SignupPage";
+import userService from "../../services/userService";
+import Weatherforcast from "../../components/Weatherforcast/Weatherforcast";
 
 class App extends Component {
   state = {
-    user: userService.getUser()
-  }
+    user: userService.getUser(),
+  };
 
   handleLogout = () => {
     userService.logout();
     this.setState({ user: null });
-  }
+  };
 
   handleSignupOrLogin = () => {
-    this.setState({user: userService.getUser()});
-  }
+    this.setState({ user: userService.getUser() });
+  };
 
-  render () {
+  render() {
     return (
       <>
-        <NavBar 
-          user={this.state.user}
-          handleLogout={this.handleLogout}
+        <NavBar user={this.state.user} handleLogout={this.handleLogout} />
+        <Route
+          exact
+          path="/signup"
+          render={({ history }) => (
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          )}
         />
-        <Route exact path='/signup' render={({ history }) => 
-          <SignupPage
-            history={history}
-            handleSignupOrLogin={this.handleSignupOrLogin}
-          />
-        }/>
-        <Route exact path='/login' render={({ history }) => 
-          <LoginPage
-            history={history}
-            handleSignupOrLogin={this.handleSignupOrLogin}
-          />
-        }/>
+        <Route
+          exact
+          path="/login"
+          render={({ history }) => (
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          )}
+        />
+        <Weatherforcast />
       </>
     );
   }
