060731aa8f5e8d81f829e8437cb02fd35f1c683a
Bsacco9814 <Brandon.Sacco13@gmail.com>
2020-07-21 17:36:19 -0500

    adding logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,13 +5,13 @@ const NavBar = ({ user, handleLogout }) => {
     <>
         <nav>
             <div className="nav-wrapper">
-                <a href="#!" class="brand-logo"><i class="material-icons">cloud</i>Weather or Not</a>
+                <a href="#!" className="brand-logo"><i className="material-icons">cloud</i>Weather or Not</a>
                 <ul id="nav-mobile" className="right">
 
-                    <li><a href="/search" className="nav-link"><i class="material-icons left">search</i>Search</a></li>
-                    <li><a href="/cities" className="nav-link"><i class="material-icons left">location_city</i>My Cities</a></li>
-                    <li><a href=" " className="nav-link"><i class="material-icons left">person</i>Welcome, {user.name}</a></li>
-                    <li><a href=" " className="nav-link" onClick={handleLogout}><i class="material-icons left">power_settings_new
+                    <li><a href="/search" className="nav-link"><i className="material-icons left">search</i>Search</a></li>
+                    <li><a href="/cities" className="nav-link"><i className="material-icons left">location_city</i>My Cities</a></li>
+                    <li><a href=" " className="nav-link"><i className="material-icons left">person</i>Welcome, {user.name}</a></li>
+                    <li><a href=" " className="nav-link" onClick={handleLogout}><i className="material-icons left">power_settings_new
 </i>Log Out</a></li>
                 </ul>
             </div>
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,67 +1,85 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-import './App.css';
-import NavBar from '../../components/NavBar/NavBar';
-import LoginPage from '../LoginPage/LoginPage';
-import SignupPage from '../SignupPage/SignupPage';
-import userService from '../../services/userService';
-import CityListPage from '../CityListPage/CityListPage';
-import SearchPage from '../SearchPage/SearchPage';
-import CityDetailsPage from '../CityDetailsPage/CityDetailsPage';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "../../components/NavBar/NavBar";
+import LoginPage from "../LoginPage/LoginPage";
+import SignupPage from "../SignupPage/SignupPage";
+import userService from "../../services/userService";
+import CityListPage from "../CityListPage/CityListPage";
+import SearchPage from "../SearchPage/SearchPage";
+import CityDetailsPage from "../CityDetailsPage/CityDetailsPage";
 
 class App extends Component {
   state = {
-    user: userService.getUser()
-  }
+    user: userService.getUser(),
+  };
 
   handleLogout = () => {
     userService.logout();
     this.setState({ user: null });
-  }
+  };
 
   handleSignupOrLogin = () => {
-    this.setState({user: userService.getUser()});
-  }
+    this.setState({ user: userService.getUser() });
+  };
 
-  render () {
+  render() {
     return (
       <>
-        <NavBar 
-          user={this.state.user}
-          handleLogout={this.handleLogout}
+        <NavBar user={this.state.user} handleLogout={this.handleLogout} />
+        {this.state.user ? (
+          ""
+        ) : (
+          <div className="logo">
+            <img src="/assets/logo.gif"></img>
+          </div>
+        )}
+        <Route
+          exact
+          path="/signup"
+          render={({ history }) => (
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          )}
         />
-        <Route exact path='/signup' render={({ history }) => 
-          <SignupPage
-            history={history}
-            handleSignupOrLogin={this.handleSignupOrLogin}
-          />
-        }/>
-        <Route exact path='/login' render={({ history }) => 
-          <LoginPage
-            history={history}
-            handleSignupOrLogin={this.handleSignupOrLogin}
-          />
-        }/>
-        <Route exact path='/cities' render={({ history }) => 
-          <CityListPage
-          history={history}
-          user={this.state.user}
-          />
-        }/>
-        <Route exact path='/search' render={({ history }) => 
-         <SearchPage
-         history={history}
-         user={this.state.user}
-         />
-        }/>
-        <Route exact path='/city/:id' render={({ history, location, match }) => 
-         <CityDetailsPage
-         match={match}
-         history={history}
-         user={this.state.user}
-         location={location}
-         />
-        }/>
+        <Route
+          exact
+          path="/login"
+          render={({ history }) => (
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/cities"
+          render={({ history }) => (
+            <CityListPage history={history} user={this.state.user} />
+          )}
+        />
+        <Route
+          exact
+          path="/search"
+          render={({ history }) => (
+            <SearchPage history={history} user={this.state.user} />
+          )}
+        />
+        <Route
+          exact
+          path="/city/:id"
+          render={({ history, location, match }) => (
+            <CityDetailsPage
+              match={match}
+              history={history}
+              user={this.state.user}
+              location={location}
+            />
+          )}
+        />
       </>
     );
   }
